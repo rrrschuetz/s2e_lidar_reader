377328adca2f6f7924d203963ae2914a3a9bcf93
Roland Schuetz <rrrschuetz@web.de>
2023-08-06 13:41:34 +0200

    compare amber and red blobs by size and render larger / closer one

--- s2e_lidar_reader_node.py
+++ s2e_lidar_reader_node.py
@@ -101,7 +101,7 @@ class s2eLidarReaderNode(Node):
         HFOV = 70.8
         VFOV = 55.6
 
-        self.get_logger().info('blob detected: %s' % msg.data)
+        #self.get_logger().info('blob detected: %s' % msg.data)
         try:
             color, y1, y2 = msg.data.split(',')
             #alphaH=(HPIX2-cxy[0])/HPIX2*HFOV/2*math.pi/180
@@ -112,7 +112,7 @@ class s2eLidarReaderNode(Node):
             idx1 = int(alphaV1/math.pi*1620)+1620
             idx2 = int(alphaV2/math.pi*1620)+1620
             self._color[idx1:idx2+1] = float(color)
-            self.get_logger().info('blob inserted: %s,%s,%s' % (color,idx1,idx2))
+            #self.get_logger().info('blob inserted: %s,%s,%s' % (color,idx1,idx2))
 
         except (SyntaxError) as e:
             self.get_logger().error('Failed to get blob coordinates: %s' % str(e))
