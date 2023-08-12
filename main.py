714991c4299e730655d681a6531af172ec85093c
Roland Schuetz <rrrschuetz@web.de>
2023-08-12 20:40:56 +0200

    compare amber and red blobs by size and render larger / closer one

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ sensor.skip_frames(time = 2000)
 
 roi = [0,0,320,200]
 thresholds_amber=[(79, 58, 29, 4, 20, 77),(82, 18, 33, 16, 27, 76)]
-#thresholds_red=[(23, 63, 7, 56, 2, 49)]
+thresholds_red=[(23, 63, 7, 56, 2, 49)]
 thresholds_white = [(100, 73, -15, 4, -15, 4)]
+thresholds_blue = [(31, 92, -19, 6, -64, -17)]
 
 while True:
     img = sensor.snapshot()
@@ -24,7 +25,7 @@ while True:
     a_pix = 0
     r_pix = 0
 
-    blobs = img.find_blobs(thresholds_amber,0,roi,pixels_threshold=30, merge=True)
+    blobs = img.find_blobs(thresholds_blue,0,roi,pixels_threshold=30, merge=True)
     if blobs:
         blobs.sort(key=lambda b: -b.pixels())
         img.draw_rectangle(blobs[0].rect())
