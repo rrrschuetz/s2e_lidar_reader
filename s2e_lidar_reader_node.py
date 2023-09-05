6827a54b1228e44cb7fc5b78fc591706aadac0e1
Roland Schuetz <rrrschuetz@web.de>
2023-09-05 21:32:49 +0200

    change to PCA 9685 - car version 2

--- s2e_lidar_reader_node.py
+++ s2e_lidar_reader_node.py
@@ -117,7 +117,7 @@ class s2eLidarReaderNode(Node):
 
         self.get_logger().info('Steering: "%s"' % str(self.servo_neutral+self._X*self.servo_ctl))
         self.get_logger().info('Power: "%s"' % str(self.neutral_pulse+self._Y))
-        #self._pwm.set_pwm(0, 0, servo_neutral+self._X*servo_ctl)
+        self._pwm.set_pwm(0, 0, servo_neutral+self._X*servo_ctl)
         #self._pwm.set_pwm(1, 0, neutral_pulse+self._Y)
 
     def openmv_h7_callback(self, msg):
