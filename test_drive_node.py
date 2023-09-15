acbb1742f010abf8c9a687e523933a965eec0e05
rrrschuetz <93436256+rrrschuetz@users.noreply.github.com>
2023-09-15 10:43:45 +0200

    Reduce logging, avoid queuing of LIDAR callback function

--- test_drive_node.py
+++ test_drive_node.py
@@ -15,7 +15,7 @@ from Adafruit_PCA9685 import PCA9685
 class testDriveNode(Node):
     HPIX = 320
     HFOV = 70.8
-    MIN_DIST = 0.15
+    MIN_DIST = 0.20
     MIN_SPEED = -0.50
     MAX_SPEED = -0.80
     reverse_pulse = 204
@@ -84,79 +84,75 @@ class testDriveNode(Node):
             self._scaler = pickle.load(f)
 
     def lidar_callback(self, msg):
-        
         if self._processing:
             self.get_logger().info('Scan skipped')
             return
         else:
             self._processing = True
             start_time = time.time()
-            self._counter += 1
             
-            if self._counter % 1 == 0:
-                try:
-                    # raw data
-                    scan = np.array(msg.ranges)
+            try:
+                # raw data
+                scan = np.array(msg.ranges)
                     
-                    # emergency brake assistant
-                    num_sections = 36
-                    section_data = np.array_split(scan, num_sections)
-                    section_means = [np.mean(section) for section in section_data]
-                    min_section_index = np.argmin(section_means)
-                    self.get_logger().info('Min distance: "%s"' % section_means[min_section_index])
-                    if section_means[min_section_index] < self.MIN_DIST:
-                        self._pwm.set_pwm(0, 0, int(self.servo_neutral))
-                        self._pwm.set_pwm(1, 0, int(self.neutral_pulse))
-                        self.get_logger().info('Emergency brake active: "%s"' % min_section_index)
-                        self._processing = False
-                        return
+                # emergency brake assistant
+                num_sections = 36
+                section_data = np.array_split(scan, num_sections)
+                section_means = [np.mean(section) for section in section_data]
+                min_section_index = np.argmin(section_means)
+                #self.get_logger().info('Min distance: "%s"' % section_means[min_section_index])
+                if section_means[min_section_index] < self.MIN_DIST:
+                    self._pwm.set_pwm(0, 0, int(self.servo_neutral))
+                    self._pwm.set_pwm(1, 0, int(self.neutral_pulse))
+                    self.get_logger().info('Emergency brake active: "%s"' % min_section_index)
+                    self._processing = False
+                    return
         
-                    scan[scan == np.inf] = np.nan
-                    x = np.arange(len(scan))
-                    finite_vals = np.isfinite(scan)
-                    scan_interpolated = np.interp(x, x[finite_vals], scan[finite_vals])
+                scan[scan == np.inf] = np.nan
+                x = np.arange(len(scan))
+                finite_vals = np.isfinite(scan)
+                scan_interpolated = np.interp(x, x[finite_vals], scan[finite_vals])
 
-                    # add color data
-                    combined = list(scan_interpolated)  # Convert to list for easier appending
-                    combined.extend(self._color)
+                # add color data
+                combined = list(scan_interpolated)  # Convert to list for easier appending
+                combined.extend(self._color)
 
-                    # add magnetometer data
-                    mag = self._sense.get_compass_raw()
-                    combined.extend([mag['x'], mag['y'], mag['z']])
-                    # add accelerometer data
-                    accel = self._sense.get_accelerometer_raw()
-                    #combined.extend([accel['x'], accel['y'], accel['z']])
-                    combined.extend([0.0,0.0,0.0])
-                    # add gyroscope data
-                    gyro = self._sense.get_gyroscope_raw()
-                    combined.extend([gyro['x'], gyro['y'], gyro['z']])
+                # add magnetometer data
+                mag = self._sense.get_compass_raw()
+                combined.extend([mag['x'], mag['y'], mag['z']])
+                # add accelerometer data
+                accel = self._sense.get_accelerometer_raw()
+                #combined.extend([accel['x'], accel['y'], accel['z']])
+                combined.extend([0.0,0.0,0.0])
+                # add gyroscope data
+                gyro = self._sense.get_gyroscope_raw()
+                combined.extend([gyro['x'], gyro['y'], gyro['z']])
                     
-                    # Reshape and standardize
-                    combined = np.reshape(combined, (1, -1))
-                    combined_standardized = self._scaler.transform(combined)
+                # Reshape and standardize
+                combined = np.reshape(combined, (1, -1))
+                combined_standardized = self._scaler.transform(combined)
 
-                    # reshape for 1D CNN input
-                    combined_standardized = np.reshape(combined_standardized, (combined_standardized.shape[0], combined_standardized.shape[1], 1))
+                # reshape for 1D CNN input
+                combined_standardized = np.reshape(combined_standardized, (combined_standardized.shape[0], combined_standardized.shape[1], 1))
 
-                    # Model prediction
-                    predictions = self._model.predict(combined_standardized)
-                    self._X = predictions[0, 0]
-                    self._Y = min(max(predictions[0, 1],self.MAX_SPEED),self.MIN_SPEED)
-                    self.get_logger().info('Predicted axes: "%s"' % predictions)
+                # Model prediction
+                predictions = self._model.predict(combined_standardized)
+                self._X = predictions[0, 0]
+                self._Y = min(max(predictions[0, 1],self.MAX_SPEED),self.MIN_SPEED)
+                #self.get_logger().info('Predicted axes: "%s"' % predictions)
 
-                    #self.get_logger().info('Steering: "%s"' % str(self.servo_neutral + self._X * self.servo_ctl))
-                    #self.get_logger().info('Power: "%s"' % str(self.neutral_pulse + self._Y * 40))
-
-                    self._pwm.set_pwm(0, 0, int(self.servo_neutral+self._X*self.servo_ctl))
-                    self._pwm.set_pwm(1, 0, int(self.neutral_pulse+self._Y*40))
+                #self.get_logger().info('Steering: "%s"' % str(self.servo_neutral + self._X * self.servo_ctl))
+                #self.get_logger().info('Power: "%s"' % str(self.neutral_pulse + self._Y * 40))
+                self._pwm.set_pwm(0, 0, int(self.servo_neutral+self._X*self.servo_ctl))
+                self._pwm.set_pwm(1, 0, int(self.neutral_pulse+self._Y*40))
         
-                except ValueError as e:
-                    self.get_logger().error('Model rendered nan: %s' % str(e))
+            except ValueError as e:
+                self.get_logger().error('Model rendered nan: %s' % str(e))
 
             self._end_time = time.time() 
-            self.get_logger().info('Scans processed "%s"' % self._counter)
-            self.get_logger().info('Cycle time: "%s" seconds' % (self._end_time-self._start_time))
-            self.get_logger().info('Step time: "%s" seconds' % (self._end_time-start_time))
+            #self.get_logger().info('Scans processed "%s"' % self._counter)
+            #self.get_logger().info('Cycle time: "%s" seconds' % (self._end_time-self._start_time))
+            #self.get_logger().info('Step time: "%s" seconds' % (self._end_time-start_time))
             self._start_time = self._end_time
             self._processing = False
        
@@ -167,10 +163,9 @@ class testDriveNode(Node):
         self._Y = msg.axes[1]
         
         self.get_logger().info('Steering: "%s"' % str(self.servo_neutral+self._X*self.servo_ctl))
-        self.get_logger().info('Power: "%s"' % str(self.neutral_pulse+self._Y*40))
-                    
-        #self._pwm.set_pwm(0, 0, int(self.servo_neutral+self._X*self.servo_ctl))
-        #self._pwm.set_pwm(1, 0, int(self.neutral_pulse+self._Y*40))
+        self.get_logger().info('Power: "%s"' % str(self.neutral_pulse+self._Y*40))     
+        self._pwm.set_pwm(0, 0, int(self.servo_neutral+self._X*self.servo_ctl))
+        self._pwm.set_pwm(1, 0, int(self.neutral_pulse+self._Y*40))
 
     def openmv_h7_callback(self, msg):
         blue = (0,0,255)
