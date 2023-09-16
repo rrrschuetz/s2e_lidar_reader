dc999ce70b824e8eaa130210bf710d29ec533852
Roland Schuetz <rrrschuetz@web.de>
2023-09-16 19:28:47 +0200

    bug fixing

--- test_drive_node.py
+++ test_drive_node.py
@@ -183,7 +183,7 @@ class testDriveNode(Node):
 
                 #self.get_logger().info('Steering: "%s"' % str(self.servo_neutral + self._X * self.servo_ctl))
                 #self.get_logger().info('Power: "%s"' % str(self.neutral_pulse + self._Y * 40))
-                self._pwm.set_pwm(0, 0, int(self.servo_neutral+self._X*self.servo_ctl*0.8))
+                self._pwm.set_pwm(0, 0, int(self.servo_neutral+self._X*self.servo_ctl))
                 self._pwm.set_pwm(1, 0, int(self.neutral_pulse+self._Y*40))
         
             except ValueError as e:
