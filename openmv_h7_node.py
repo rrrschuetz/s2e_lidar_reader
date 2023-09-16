a081fe197da4f82678b23c1b402ec4b1b1fe7e15
Roland Schuetz <rrrschuetz@web.de>
2023-09-16 15:29:41 +0200

    bug fixing

--- openmv_h7_node.py
+++ openmv_h7_node.py
@@ -11,14 +11,17 @@ class openmvH7Node(Node):
         self.serial_port = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
         self.timer = self.create_timer(0.1, self.timer_callback)  # Adjust the timer callback rate as needed
         self.get_logger().info('OpenMV H7 connected' )
-
     def timer_callback(self):
-        msg = String()
-        if self.serial_port.in_waiting:
-            msg.data = self.serial_port.readline().decode('utf-8').strip()  # Read a line and strip it
-            self.publisher_.publish(msg)
-            #self.get_logger().info('blob published %s' % msg.data )
-
+        try:
+            msg = String()
+            if self.serial_port.in_waiting:
+                msg.data = self.serial_port.readline().decode('utf-8').strip()  # Read a line and strip it
+                self.publisher_.publish(msg)
+                #self.get_logger().info('blob published %s' % msg.data )
+        except serial.SerialException as e:
+            self.get_logger().error(f"Serial Exception: {e}")
+        except OSError as e:
+            self.get_logger().error(f"OS Error: {e}")
 
 def main(args=None):
     rclpy.init(args=args)
