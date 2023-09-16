4e3dcf13e2889d53a98f3eadc28b4bed74cd466e
Roland Schuetz <rrrschuetz@web.de>
2023-09-16 14:57:01 +0200

    bug fixing

--- test_drive_node.py
+++ test_drive_node.py
@@ -193,13 +193,11 @@ class testDriveNode(Node):
 
             self._end_time = self.get_clock().now()
             call_age = (self._end_time - self._start_time).nanoseconds * 1e-9
-            self._custom_logger.info('Cycle age: {:.4f} seconds, Call age: {:.4f}, Message age: {:.4f} seconds', cycle_age,call_age,message_age)
+            self._custom_logger.info('Cycle age: %.4f seconds, Call age: %.4f, Message age: %.4f seconds', cycle_age, call_age, message_age)
+
             self._processing = False
        
     def joy_callback(self, msg):
-        self.get_logger().info('Buttons: "%s"' % msg.buttons)
-        self.get_logger().info('Axes: "%s"' % msg.axes)
-
         if hasattr(msg, 'buttons') and len(msg.buttons) > 0:
             # Check if 'A' button is pressed - switch on AI steering
             if msg.buttons[0] == 1:
