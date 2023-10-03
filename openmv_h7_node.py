bc5d160321b5bdc78a6f3c37b274aa85b8015111
Roland Schuetz <rrrschuetz@web.de>
2023-10-03 18:20:31 +0200

    img logging

--- openmv_h7_node.py
+++ openmv_h7_node.py
@@ -10,12 +10,18 @@ class openmvH7Node(Node):
         self.serial_port = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
         self.timer = self.create_timer(0.1, self.timer_callback)  # Adjust the timer callback rate as needed
         self.get_logger().info('OpenMV H7 connected' )
+        self._counter = 0
     def timer_callback(self):
         try:
             msg = String()
             if self.serial_port.in_waiting:
-                msg.data = self.serial_port.read(self.serial_port.in_waiting).decode()
-                #msg.data = self.serial_port.readline().decode('utf-8').strip()  # Read a line and strip it
+                header = self.serial_port.readline().decode().strip()
+                _, str_len, _, jpg_len = map(int, header.split(','))
+                msg.data = self.serial_port.read(str_len).decode()
+                with open("/home/rrrschuetz/test/saved_images/image_{}.jpg".format(counter),'wb') as f:
+                    f.write(self.serial_read.read(jpg_len))
+                    self._counter += 1
+                    if self._counter > 9999: self._counter = 0
                 self.publisher_.publish(msg)
                 self.get_logger().info('blob published %s' % msg.data )
         except serial.SerialException as e:
