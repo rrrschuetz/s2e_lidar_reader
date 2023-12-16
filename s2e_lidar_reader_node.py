93c25aeb2ff911181a67ab557c521a4243eb2c92
Roland Schuetz <rrrschuetz@web.de>
2023-12-16 23:14:39 +0100

    img logging

--- s2e_lidar_reader_node.py
+++ s2e_lidar_reader_node.py
@@ -20,7 +20,7 @@ class s2eLidarReaderNode(Node):
     servo_min = 240  # Min pulse length out of 4096
     servo_max = 375  # Max pulse length out of 4096
     servo_neutral = int((servo_max+servo_min)/2)
-    servo_ctl = int((servo_max-servo_min)/2 *1.5)
+    servo_ctl = int(-(servo_max-servo_min)/2 *1.5)
     motor_ctl = 40
 
     def __init__(self):
