4a23b993c3610b314368462277c72224231db58f
Roland Schuetz <rrrschuetz@web.de>
2023-12-23 22:32:14 +0100

    img logging

--- openmv_h7_node1.py
+++ openmv_h7_node1.py
@@ -13,9 +13,9 @@ class openmvH7Node(Node):
             script_data = file.read()
             self.serial_port.write(script_data)
             self.get_logger().info('OpenMV H7 1 script sent' )
-        #time.sleep(10)
-        #self.serial_port.reset_input_buffer()
-        #self.serial_port.reset_output_buffer()
+        time.sleep(10)
+        self.serial_port.reset_input_buffer()
+        self.serial_port.reset_output_buffer()
 
         self.timer = self.create_timer(0.1, self.timer_callback)  # Adjust the timer callback rate as needed
         self._counter = 0
