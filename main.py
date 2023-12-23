0b6afa966b2d4761f3550fae305ca927ef0e6ec3
Roland Schuetz <rrrschuetz@web.de>
2023-12-23 22:54:08 +0100

    img logging

--- main.py
+++ main.py
@@ -7,12 +7,9 @@ green_led = pyb.LED(2)
 blue_led = pyb.LED(3)
 
 def receive_script(filename):
-    while not usb.isconnected(): pass
-
-
-    if usb.any(): with open(filename, 'wb') as file:
-            data = usb.recv(4096)  # Receive 64 bytes at a time
-            file.write(data)
+    if usb.isconnected() and usb.any(): with open(filename, 'wb') as file:
+        data = usb.recv(4096)  # Receive 64 bytes at a time
+        file.write(data)
 
 # Name of the new script file
 new_script_filename = "/h7_cam_exec.py"
