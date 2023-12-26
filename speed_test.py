a7e76bda67d1fe0f39346daa57f4788c91cf49a8
Roland Schuetz <rrrschuetz@web.de>
2023-12-26 16:07:16 +0100

    img logging

--- speed_test.py
+++ speed_test.py
@@ -2,9 +2,9 @@ from sense_hat import SenseHat
 import csv
 import math, numpy as np
 
+velocity = [0]
+acceleration = [0]
 dt = 0.1
-velocity = [0.0,0.0]
-acceleration = [0.0,0.0]
 min_x = 0
 min_y = 0
 max_x = 0
@@ -23,15 +23,16 @@ with open('/home/rrrschuetz/test/speed_data.csv', 'w', newline='') as file:
 
     while True:
         accel = sense.get_accelerometer_raw()
-        acceleration = [accel['x'],accel['y']]
-        min_x = min(min_x,accel['x'])
-        max_x = max(max_x,accel['x'])
-        min_y = min(min_y,accel['y'])
-        max_y = max(max_y,accel['y'])
+        x=accel['x']
+        y=accel['y']
+        min_x = min(min_x,x)
+        max_x = max(max_x,x)
+        min_y = min(min_y,y)
+        max_y = max(max_y,y)
         velocity = [v + a * dt for v, a in zip(velocity, acceleration)]
         speed = sum(v**2 for v in velocity)**0.5
-        print(f'current speed m/s: {speed:.6f}, min_x: {min_x:.6f}, max_x: {max_x:.6f}, min_y: {min_y:.6f}, max_y: {max_y:.6f}')
+        print(f'current speed m/s: {speed:.6f}, min_y: {min_y:.6f}, max_y: {max_y:.6f}, y: {y:.6f}')
 
         # Write data to CSV
-        writer.writerow([f'{speed:.6f}', f'{min_x:.6f}', f'{max_x:.6f}', f'{min_y:.6f}', f'{max_y:.6f}'])
+        writer.writerow([f'{speed:.6f}', f'{min_y:.6f}', f'{max_y:.6f}', f'{y:.6f}'])
 
