9821271aef82b7dbf5c13dfef1c9ee462b0d2715
Roland Schuetz <rrrschuetz@web.de>
2023-12-26 12:49:09 +0100

    img logging

--- speed_test.py
+++ speed_test.py
@@ -1,30 +1,36 @@
-import rclpy, math
-from rclpy.time import Time
-from rclpy.node import Node
-from rclpy.executors import SingleThreadedExecutor
-from rclpy.qos import QoSProfile, QoSHistoryPolicy, QoSReliabilityPolicy, QoSDurabilityPolicy
-from sensor_msgs.msg import LaserScan
-from sensor_msgs.msg import Joy
-from std_msgs.msg import String
 from sense_hat import SenseHat
-import numpy as np
-import pickle
-import logging
-from logging.handlers import RotatingFileHandler
-from Adafruit_PCA9685 import PCA9685
+import math, numpy as np
 
 dt = 0.1
-velocity = [0.0,0.0,0.0]
-acceleration = [0.0,0.0,0.0]
+velocity = [0.0,0.0]
+acceleration = [0.0,0.0]
+min_x = 0
+min_y = 0
+max_x = 0
+max_y = 0
 
 # Initialize sense hat
 sense = SenseHat()
 sense.clear()
 sense.show_message("OK", text_colour=[255, 0, 0])
 
-While True:
-    accel = sense.get_accelerometer_raw()
-    acceleration = [ accel['x'],accel['y'],accel['z']]
-    velocity = [v + a * dt for v, a in zip(velocity, acceleration)]
-    speed = sum(v**2 for v in velocity)**0.5
-    get_logger().info('current speed m/s: "%s"' % speed)
+# Open a CSV file to write data
+with open('/home/rrrschuetz/test/speed_data.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    # Write CSV header
+    writer.writerow(['Speed (m/s)', 'Min X', 'Max X', 'Min Y', 'Max Y'])
+
+    while True:
+        accel = sense.get_accelerometer_raw()
+        acceleration = [accel['x'],accel['y']]
+        min_x = min(min_x,accel['x'])
+        max_x = max(max_x,accel['x'])
+        min_y = min(min_y,accel['y'])
+        max_y = max(max_y,accel['y'])
+        velocity = [v + a * dt for v, a in zip(velocity, acceleration)]
+        speed = sum(v**2 for v in velocity)**0.5
+        print(f'current speed m/s: {speed:.4f}, min_x: {min_x:.4f}, max_x: {max_x:.4f}, min_y: {min_y:.4f}, max_y: {max_y:.4f}')
+
+        # Write data to CSV
+        writer.writerow([f'{speed:.4f}', f'{min_x:.4f}', f'{max_x:.4f}', f'{min_y:.4f}', f'{max_y:.4f}'])
+
