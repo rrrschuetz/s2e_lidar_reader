3fd52b1406e0c2021d2aa34fbc28c6488aa8e6b4
Roland Schuetz <rrrschuetz@web.de>
2023-12-27 17:14:24 +0100

    speed monitor

--- speed_monitor_node.py
+++ speed_monitor_node.py
@@ -16,9 +16,9 @@ class SpeedMonitorNode(Node):
         self.publisher = self.create_publisher(String, 'speed', 10)
 
         # Initialize variables for speed calculation
-        self.count = 0
-        self.last_time = None
-        self.distance_per_rotation = 0.15  # Set the distance covered per rotation (e.g., circumference of a wheel)
+        self._count = 0
+        self._last_time = None
+        self._distance_per_rotation = 0.15  # Set the distance covered per rotation (e.g., circumference of a wheel)
 
         # Set up a GPIO event detect
         GPIO.add_event_detect(self.gpio_pin, GPIO.FALLING, callback=self.pin_callback)
@@ -26,20 +26,20 @@ class SpeedMonitorNode(Node):
     def pin_callback(self, channel):
         current_time = time.time()
         if self.last_time is not None:
-            time_interval = current_time - self.last_time
+            time_interval = current_time - self._last_time
             speed = self.calculate_speed(time_interval)
             self.publish_speed(speed)
-        self.last_time = current_time
+        self._last_time = current_time
 
         # Increment count on every falling edge (HIGH to LOW transition)
-        self.count += 1
-        speed = self.calculate_speed()
+        self._count += 1
+        speed = self.calculate_speed(time_interval)
         self.publish_speed(speed)
 
     def calculate_speed(self, time_interval):
         # Speed = Distance / Time
         if time_interval > 0:
-            return self.distance_per_rotation / time_interval
+            return self._distance_per_rotation / time_interval
         else:
             return 0
 
