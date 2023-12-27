35db8c8b0a4605a2ce8404684a019f85300c7dfd
Roland Schuetz <rrrschuetz@web.de>
2023-12-27 10:42:51 +0100

    speed monitoring

--- s2e_lidar_reader_node.py
+++ s2e_lidar_reader_node.py
@@ -240,7 +240,7 @@ class s2eLidarReaderNode(Node):
                 self._color2[cx1:cx2+1] = fcol
                 #self.get_logger().info('blob inserted: %s,%s,%s' % (color,x1,x2))
 
-   def speed_monitor_callback(self, msg):
+    def speed_monitor_callback(self, msg):
         self.get_logger().info('Speed monitor: "%s" m/s' % msg)
         self._speed = eval(msg.data)
 
