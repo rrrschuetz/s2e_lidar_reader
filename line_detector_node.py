e6060418a00b43d8f1addf3563feaa8ea03ff1e6
Roland Schuetz <rrrschuetz@web.de>
2023-12-28 20:50:25 +0100

    speed monitor

--- line_detector_node.py
+++ line_detector_node.py
@@ -9,7 +9,8 @@ class LineDetectorNode(Node):
         self.publisher = self.create_publisher(Bool, 'line_detector', 10)
         self.sensor_pin = 22  # Change as per your GPIO connection
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.sensor_pin, GPIO.IN)
+        #GPIO.setup(self.sensor_pin, GPIO.IN)
+        GPIO.setup(self.gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
         # Timer to read the sensor value every second
         self.timer = self.create_timer(1.0, self.timer_callback)
