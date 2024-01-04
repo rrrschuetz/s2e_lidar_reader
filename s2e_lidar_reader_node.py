a8d473ed839077b5e1018123a5dc2a70e3b5dbd8
Roland Schuetz <rrrschuetz@web.de>
2024-01-04 23:52:51 +0100

    improve color vision

--- s2e_lidar_reader_node.py
+++ s2e_lidar_reader_node.py
@@ -117,7 +117,7 @@ class s2eLidarReaderNode(Node):
 
     def lidar_callback(self, msg):
         # Convert the laser scan data to a string
-        scan = np.array(msg.ranges[:self.num_scan2]+msg.ranges[self.num_scan2:])
+        scan = np.array(msg.ranges[self.num_scan2:]+msg.ranges[:self.num_scan2])
         #scan = np.array(msg.ranges[self.num_scan2:self.num_scan2+self.num_scan])
         #scan[scan == np.inf] = 0.0
         scan[scan == np.inf] = np.nan
