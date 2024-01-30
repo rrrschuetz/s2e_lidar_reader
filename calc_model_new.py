0b7555da4708387618bf1faf44fd7807e852ab42
Roland Schuetz <rrrschuetz@web.de>
2024-01-30 01:00:00 +0100

    fix data transport from camera

--- calc_model_new.py
+++ calc_model_new.py
@@ -102,7 +102,7 @@ def create_cnn_model(lidar_input_shape, color_input_shape):
 
     # Concatenation
     #concatenated = Concatenate()([lidar_path, color_path])
-    concatenated = WeightedConcatenate(weight_lidar=0.3, weight_color=0.7)([lidar_path, color_path])
+    concatenated = WeightedConcatenate(weight_lidar=0.1, weight_color=0.9)([lidar_path, color_path])
 
     # Further processing
     combined = Dense(64, activation='relu')(concatenated)
