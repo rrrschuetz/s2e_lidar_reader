d680f7bfadf9c4e09abd02c076b4fadc063d8d03
Roland Schuetz <rrrschuetz@web.de>
2024-02-04 21:02:04 +0100

    fix data transport from camera

--- s2e_lidar_reader_parking_node.py
+++ s2e_lidar_reader_parking_node.py
@@ -142,7 +142,7 @@ class s2eLidarReaderParkingNode(Node):
             self.get_logger().error('IOError I2C occurred: %s' % str(e))
 
     def openmv_h7_callback1(self, msg):
-        #self.get_logger().info('CAM1 msg received: "%s"' % msg)
+        self.get_logger().info('CAM1 msg received: "%s"' % msg)
         self._color1_m = np.zeros(self.HPIX, dtype=int)
 
         data = msg.data.split(',')
@@ -163,7 +163,7 @@ class s2eLidarReaderParkingNode(Node):
                 self._color1_m[x1:x2] = self.WEIGHT
 
     def openmv_h7_callback2(self, msg):
-        #self.get_logger().info('CAM2 msg received: "%s"' % msg)
+        self.get_logger().info('CAM2 msg received: "%s"' % msg)
         self._color2_m = np.zeros(self.HPIX, dtype=int)
 
         data = msg.data.split(',')
