768f2ec01a2d3cf7dadbbef02d2ab09e8580c090
Roland Schuetz <rrrschuetz@web.de>
2024-02-06 00:38:44 +0100

    fix data transport from camera

--- h7_cam_exec.py
+++ h7_cam_exec.py
@@ -32,7 +32,8 @@ green =  (30, 100, -64, -8, -32, 32)  # generic green
 #red = (40, 74, 23, 84, 13, 62)
 red =  (30, 100, 15, 127, 15, 127)    # generic red
 
-magenta = (0, 59, 40, 68, 127, -128)
+#magenta = (0, 59, 40, 68, 127, -128)
+magenta = (0, 71, 45, 127, 127, -94)
 
 thresholds=[green, red, magenta]
 roi = [0,0,320,140]
