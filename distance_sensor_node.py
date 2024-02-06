cad8e83e18556370f3a4c491413003e64060bc3b
Roland Schuetz <rrrschuetz@web.de>
2024-02-06 23:37:51 +0100

    fix data transport from camera

--- distance_sensor_node.py
+++ distance_sensor_node.py
@@ -10,20 +10,21 @@ class DistanceSensorNode(Node):  # Corrected class name
         self.publisher = self.create_publisher(Float32, 'distance_sensor', 10)  # Corrected message type
 
         # Create a VL53L1X object
-        self.tof = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)  # Default I2C address is 0x29
+        #self.tof = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)  # Default I2C address is 0x29
         # Open and start ranging
-        self.tof.open()
+        #self.tof.open()
         #self.tof.start_ranging(VL53L1X.VL53L1X_LONG_RANGE_MODE)  # Choose ranging mode
-        self.tof.start_ranging(1)  # Choose ranging mode
+        #self.tof.start_ranging(1)  # Choose ranging mode
         self.timer = self.create_timer(0.1, self.timer_callback)  # Check every 0.1 second
 
     def __del__(self):
-        self.tof.stop_ranging()  # Stop ranging
-        self.tof.close()
+        #self.tof.stop_ranging()  # Stop ranging
+        #self.tof.close()
 
     def timer_callback(self):  # Removed unused parameter
         msg = Float32()
-        msg.data = self.tof.get_distance()  # Corrected to use self to access tof
+        #msg.data = self.tof.get_distance()  # Corrected to use self to access tof
+        msg.data = 1.0
         self.publisher.publish(msg)
         self.get_logger().info('Publishing: Distance in mm "%f"' % msg.data)  # Corrected logging format
 
