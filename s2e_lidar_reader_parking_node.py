bf064a2569faae3990eb696e2586bc2a1bb0028f
Roland Schuetz <rrrschuetz@web.de>
2024-02-10 16:06:01 +0100

    fix data transport from camera

--- s2e_lidar_reader_parking_node.py
+++ s2e_lidar_reader_parking_node.py
@@ -26,7 +26,7 @@ class s2eLidarReaderParkingNode(Node):
     servo_neutral = int((servo_max+servo_min)/2)
     servo_ctl = int(-(servo_max-servo_min)/2 *1.7)
     motor_ctl_fwd = 14
-    motor_ctl_rev = 14
+    motor_ctl_rev = 10
     relay_pin = 17
     WEIGHT = 1
 
