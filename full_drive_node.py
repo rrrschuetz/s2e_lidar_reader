44fbb0cf7514f0563e9b739c1cd72e413195dc4d
Roland Schuetz <rrrschuetz@web.de>
2024-02-24 15:41:05 +0100

    fix data transport from camera

--- full_drive_node.py
+++ full_drive_node.py
@@ -229,6 +229,8 @@ class fullDriveNode(Node):
                             self.get_logger().info("Race completed!")
                             self._state = "PARK"
                             self._processing = False
+                            msg.data = "Race completed, parking mode"
+                            self.publisher_.publish(msg)
                             return
 
                 self._clockwise = (self._total_heading_change > 0)
