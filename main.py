90fdeb15904a7e52b3b9417116ba452a1e573539
rrrschuetz <93436256+rrrschuetz@users.noreply.github.com>
2024-03-11 18:49:58 +0100

    Update main.py
    Adapt to RT 1062 camera

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
-import pyb, time
-import os
+#import pyb
+import os, time
+from machine import Pin
 
-usb = pyb.USB_VCP()
-red_led = pyb.LED(1)
-green_led = pyb.LED(2)
-blue_led = pyb.LED(3)
+#usb = pyb.USB_VCP()
+#red_led = pyb.LED(1)
+#green_led = pyb.LED(2)
+#blue_led = pyb.LED(3)
+#red_led.off()
+#green_led.on()
+#blue_led.off()
 
-red_led.off()
-green_led.on()
-blue_led.off()
+red_led = Pin('P0', Pin.OUT)
+green_led = Pin('P1', Pin.OUT)
+blue_led = Pin('P2', Pin.OUT)
+red_led.value(0)  # 1 to turn on, 0 to turn off
+green_led.value(1)
+blue_led.value(0)
 
 def receive_script(filename):
-    while not usb.isconnected():
-        pass
+    #while not usb.isconnected():
+    #    pass
 
-    red_led.off()
-    green_led.off()
-    blue_led.on()
+    #red_led.off()
+    #green_led.off()
+    #blue_led.on()
+    red_led.value(0)  # 1 to turn on, 0 to turn off
+    green_led.value(0)
+    blue_led.value(1)
 
-    while not usb.any():
-        pass  # Wait for data
+    #while not usb.any():
+    #    pass  # Wait for data
 
-    with open(filename, 'wb') as file:
-        while usb.any():
-            data = usb.recv(4096)  # Receive 64 bytes at a time
-            file.write(data)
+    #with open(filename, 'wb') as file:
+    #    while usb.any():
+    #        data = usb.recv(4096)  # Receive 64 bytes at a time
+    #        file.write(data)
 
 # Name of the new script file
 new_script_filename = "/h7_cam_exec.py"
