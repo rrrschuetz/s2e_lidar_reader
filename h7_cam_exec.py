088928f901acb13b16159ecb62192619369d31eb
Roland Schuetz <rrrschuetz@web.de>
2024-04-25 02:38:48 +0200

    CLOCKWISE in display

--- h7_cam_exec.py
+++ h7_cam_exec.py
@@ -42,7 +42,7 @@ while True:
         #time.sleep(0.05)
         img = sensor.snapshot()
         img.lens_corr(strength=2.6, zoom=1.0)
-        img.gamma_corr(gamma = 1.0, contrast = 1.0, brightness = 0.3)
+        img.gamma_corr(gamma = 1.0, contrast = 1.0, brightness = 0.2)
         #img.laplacian(2, sharpen=True)
 
         blob_entries = []
