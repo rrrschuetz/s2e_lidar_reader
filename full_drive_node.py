1899599bb667c889d7320d9ead95a7b4d58c71ff
Roland Schuetz <rrrschuetz@web.de>
2024-04-28 20:38:14 +0200

    improve gyro accuracy

--- full_drive_node.py
+++ full_drive_node.py
@@ -247,7 +247,7 @@ class fullDriveNode(Node):
                     section_means = [np.mean(section) for section in section_data]
                     self._front_dist = section_means[5]
 
-                    if abs(self._total_heading_change) >= 80 and self._front_dist < 1.5:
+                    if abs(self._total_heading_change) >= 80 and self._front_dist > 1.0 and self._front_dist < 1.5:
                         self._corner_cnt +=1
                         self.get_logger().info(f"Number of corners {self._corner_cnt} heading {self._total_heading_change}")
                         self._total_heading_change = 0
