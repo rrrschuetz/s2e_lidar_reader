fb11d1a11495db2a5f8d1ff7b4ebb351f4e13928
Roland Schuetz <rrrschuetz@web.de>
2024-04-28 18:44:48 +0200

    improve gyro accuracy

--- full_drive_node.py
+++ full_drive_node.py
@@ -242,17 +242,17 @@ class fullDriveNode(Node):
                     self._total_heading_change += heading_change
                     self._last_heading = self._current_heading
 
-                    if abs(self._total_heading_change) >= 90:
-                        self._total_heading_change = 0
-                        self._corner_cnt +=1
-                        self.get_logger().info(f"Number of corners {self._corner_cnt}")
-
                     num_sections = 18
                     section_data = np.array_split(scan, num_sections)
                     section_means = [np.mean(section) for section in section_data]
                     self._front_dist = section_means[9]
 
-                    if self._parking_lot > 50 and self._corner_cnt == 13 and self._parking_lot_detect and self._front_dist < 2.0:  #430
+                    if abs(self._total_heading_change) >= 80 and self._front_dist < 1.5:
+                        self._corner_cnt +=1
+                        self.get_logger().info(f"Number of corners {self._corner_cnt} heading {self._total_heading_change}")
+                        self._total_heading_change = 0
+
+                    if self._parking_lot > 50 and self._corner_cnt == 12 and abs(self._total_heading_change) > 50 and self._parking_lot_detect and self._front_dist < 2.0:  #430
                         duration_in_seconds = (self.get_clock().now() - self._round_start_time).nanoseconds * 1e-9
                         self.get_logger().info(f"Race in {duration_in_seconds} sec completed!")
                         self.get_logger().info(f"Heading change: {self._total_heading_change} Distance: {self._front_dist}")
