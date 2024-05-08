4252e2b44007d0b4cbe73ffb389dd9754dd04d7b
Roland Schuetz <rrrschuetz@web.de>
2024-05-08 02:58:11 +0200

    move forward for clockwise detection

--- speed_control_node.py
+++ speed_control_node.py
@@ -59,7 +59,7 @@ class SpeedControlNode(Node):
         self.pid.sample_time = 0.1
 
     def move_to_impulse(self, impulse_goal):
-        self.get_logger().info("move_to_impulse called: %s" % self.impulse_goal)
+        self.get_logger().info("move_to_impulse called: %s" % impulse_goal)
         if impulse_goal <= 0:
             self.get_logger().info("move_to_impulse: No move!")
             return
