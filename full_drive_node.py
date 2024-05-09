58f185b030d44cb40093b613eeee145ca615462f
Roland Schuetz <rrrschuetz@web.de>
2024-05-09 02:50:23 +0200

    move forward for clockwise detection

--- full_drive_node.py
+++ full_drive_node.py
@@ -407,10 +407,10 @@ class fullDriveNode(Node):
             ########################
             elif self._state == 'PARK':
 
-                num_sections = 9
+                num_sections = 21
                 section_data = np.array_split(scan, num_sections)
                 section_means = [np.nanmean(section) for section in section_data]
-                self._front_dist = section_means[5]
+                self._front_dist = section_means[10]
 
                 self._current_heading = self._sense.gyro['yaw']
                 heading_change = abs(self.calculate_heading_change(self._last_heading, self._current_heading))
