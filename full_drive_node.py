a7562c443864a41c23eb863e37bfa21217e19618
Roland Schuetz <rrrschuetz@web.de>
2024-05-10 00:51:42 +0200

    move forward for clockwise detection

--- full_drive_node.py
+++ full_drive_node.py
@@ -323,7 +323,7 @@ class fullDriveNode(Node):
                         self._obstacle_chk = True
 
                         if not self.initial_race and (min_far_dist < 0.8 or min_near_dist < 0.2):
-                            self.get_logger().info(f"Obstacle: {min_far_dist}, {min_near_dist}, distance: {self._front_dist}, moving backward: {X} {M}")
+                            self.get_logger().info(f"Obstacle: {min_far_dist}, {min_near_dist}, distance: {self._front_dist}")
                             self.steer(-1.0 if not self._clockwise else 1.0,True)
                             self.move("R15")
                             H = -45 if not self._clockwise else 45
