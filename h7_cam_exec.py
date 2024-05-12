55ab654d49ccce76906fab71497af5bf0595c550
Roland Schuetz <rrrschuetz@web.de>
2024-05-12 16:43:07 +0200

    introduce config.ini

--- h7_cam_exec.py
+++ h7_cam_exec.py
@@ -4,11 +4,11 @@ import machine, datetime
 #db_gain = "20"
 #gamma_corr = "1.2"
 
-try:
-    with open('/h7_cam_exec.log', 'w') as file:
-        file.write(f"db_gain {db_gain}, gamma_corr {gamma_corr} \n")
-except:
-    pass
+#try:
+#    with open('./h7_cam_exec.log', 'w') as file:
+#        file.write(f"db_gain {db_gain}, gamma_corr {gamma_corr} \n")
+#except:
+#    pass
 
 # Get the unique machine ID
 unique_id = machine.unique_id()
