3f3e174a104ed703d495a1d18de590a6ab512a50
Roland Schuetz <rrrschuetz@web.de>
2024-05-12 21:47:44 +0200

    introduce config.ini

--- full_drive_node.py
+++ full_drive_node.py
@@ -537,7 +537,7 @@ class fullDriveNode(Node):
                     if cam == 2: self._color2_m[ix1:ix2] = self.WEIGHT
                     self._parking_lot += 1
 
-                #self.get_logger().info('CAM: blob inserted: %s,%s,%s,%s' % (cam,color,x1,x2))
+                self.get_logger().info('CAM: blob inserted: %s,%s,%s,%s' % (cam,color,x1,x2))
 
         except:
             self.get_logger().error('Faulty cam msg received: "%s"' % msg)
