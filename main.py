3afa5313337e6d9c61fcb6369c6d2af6aed89f0c
Roland Schuetz <rrrschuetz@web.de>
2024-05-14 00:55:25 +0200

    introduce config.ini

--- main.py
+++ main.py
@@ -1,54 +1,40 @@
 import time, pyb
-import hashlib
 
 class USBReceiver:
     def __init__(self, usb):
         self.usb = usb
 
-    def wait_for_connection(self, timeout=120):
-        start_time = time.time()
+    def wait_for_connection(self):
         while not self.usb.isconnected():
             time.sleep(0.1)
-            if time.time() - start_time > timeout:
-                raise Exception("Timeout waiting for USB connection.")
 
-    def read_line(self, timeout=120):
+    def read_line(self):
         line = ''
-        start_time = time.time()
         while True:
-            if self.usb.any():
-                char = self.usb.recv(1).decode()
-                if char == '\n':
-                    break
-                line += char
-            else:
-                if time.time() - start_time > timeout:
-                    raise Exception("Timeout reading line from USB.")
-                time.sleep(0.1)
+            char = self.usb.recv(1).decode()
+            if char == '\n':
+                break
+            line += char
         return line.strip()
 
     def receive_script(self, filename):
         self.wait_for_connection()
+
         params = {}
         with open(filename, 'wb') as file:
             params['db_gain'] = self.read_line()
             params['gamma_corr'] = self.read_line()
-            expected_hash = self.read_line()
             length = int(self.read_line())
 
-            received_data = bytearray()
-            while len(received_data) < length:
-                data_needed = length - len(received_data)
+            count = 0
+            while count < length:
+                data_needed = length - count
                 data = self.usb.recv(min(64, data_needed))
                 if data:
-                    received_data.extend(data)
+                    file.write(data)
+                    count += len(data)
                 else:
                     time.sleep(0.1)  # Pause briefly to wait for more data
-
-            received_hash = hashlib.sha256(received_data).hexdigest()
-            if received_hash != expected_hash:
-                raise ValueError("Data corruption: hash mismatch.")
-            file.write(received_data)
 
         return params
 
@@ -57,9 +43,6 @@ if __name__ == '__main__':
     usb = pyb.USB_VCP()
     receiver = USBReceiver(usb)
     new_script_filename = '/h7_cam_exec.py'
-    try:
-        params = receiver.receive_script(new_script_filename)
-        globals().update(params)
-        exec(open(new_script_filename).read(), globals())
-    except Exception as e:
-        pass
+    params = receiver.receive_script(new_script_filename)
+    globals().update(params)
+    exec(open(new_script_filename).read(), globals())
--- openmv_h7_node1.py
+++ openmv_h7_node1.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 import serial, time
-import hashlib, configparser
+import configparser
 
 class openmvH7Node(Node):
     def __init__(self):
@@ -20,9 +20,7 @@ class openmvH7Node(Node):
 
         with open("/home/rrrschuetz/ros2_ws4/src/s2e_lidar_reader/s2e_lidar_reader/h7_cam_exec.py", 'rb') as file:
             script_data = file.read()
-            send_data = bytearray()
-            send_data.extend(script_data)
-            header_data = f"{db_gain}\n{gamma_corr}\n{hashlib.sha256(send_data).hexdigest()}\n{len(script_data)}\n".encode('utf-8')
+            header_data = f"{db_gain}\n{gamma_corr}\n{len(script_data)}\n".encode('utf-8')
             self.serial_port.write(header_data + script_data)
             self.get_logger().info(f"OpenMV H7 1 script sent: {header_data}" )
         #time.sleep(10)
