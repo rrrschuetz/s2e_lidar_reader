8eff355d028500f6e1a36171a41d9df431b54c09
Roland Schuetz <rrrschuetz@web.de>
2024-05-14 19:51:43 +0200

    introduce config.ini

--- full_drive_node.py
+++ full_drive_node.py
@@ -469,8 +469,8 @@ class fullDriveNode(Node):
         try:
             data = msg.data.split(',')
 
-            if data[0] == '33001c000851303436373730': cam = 1     # 33001c000851303436373730
-            elif data[0] == '2d0024001951333039373338': cam = 2   # 340046000e51303434373339
+            if data[0] == '33001c000851303436373730': cam = 1
+            elif data[0] == '2d0024001951333039373338': cam = 2
             else: return
 
             if cam == 1:
@@ -512,7 +512,7 @@ class fullDriveNode(Node):
                     if cam == 2: self._color2_m[ix1:ix2] = self.WEIGHT
                     self._parking_lot += 1
 
-                self.get_logger().info('CAM: blob inserted: %s,%s,%s,%s' % (cam,color,x1,x2))
+                #self.get_logger().info('CAM: blob inserted: %s,%s,%s,%s' % (cam,color,x1,x2))
 
         except:
             self.get_logger().error('Faulty cam msg received: "%s"' % msg)
