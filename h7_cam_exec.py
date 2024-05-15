dc48861ba11bc3a29600296878bf14cfd699dcae
Roland Schuetz <rrrschuetz@web.de>
2024-05-15 07:40:46 +0200

    increase camera message throughput

--- h7_cam_exec.py
+++ h7_cam_exec.py
@@ -67,14 +67,14 @@ while True:
 
         bloblist = ','.join(blob_entries)
         if bloblist:
-
+            usb.flush()
             #jpg = img.compress(quality=85)  # Compress image into JPEG format
             #header = "STR,{},STR,{},JPG,{}\n".format(unique_id_hex, len(bloblist), len(jpg))
             usb.write("{},".format(unique_id_hex))
             usb.write(bloblist)
             usb.write("\n")
             #usb.write(jpg)
-            usb.flush()
+            #usb.flush()
 
     except Exception as e:
         pass
