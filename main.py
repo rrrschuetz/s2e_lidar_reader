dff49bd183071976cdc0c2302fa04a781ed3a491
Roland Schuetz <rrrschuetz@web.de>
2024-05-15 22:44:56 +0200

    increase camera message throughput

--- main.py
+++ main.py
@@ -38,12 +38,14 @@ class USBReceiver:
                 params['db_gain'] = self.read_line()
                 params['gamma_corr'] = self.read_line()
                 length = int(self.read_line())
+                log_message(f"Parameters received: {params}")
                 log_message(f"Receiving script of length: {length}")
 
                 count = 0
                 while count < length:
                     if self.usb.any():
                         data_needed = length - count
+                        log_message(f"Data needed: {data_needed}")
                         data = self.usb.recv(min(64, data_needed))
                         if data:
                             file.write(data)
@@ -63,6 +65,5 @@ if __name__ == '__main__':
     receiver = USBReceiver(usb)
     new_script_filename = '/h7_cam_exec.py'
     params = receiver.receive_script(new_script_filename)
-    log_message(f"Parameters received: {params}")
     globals().update(params)
     exec(open(new_script_filename).read(), globals())
