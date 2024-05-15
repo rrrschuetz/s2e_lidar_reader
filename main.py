cc0ecfe3e647e3ed33df284efe9b2f9688093311
Roland Schuetz <rrrschuetz@web.de>
2024-05-16 00:19:54 +0200

    increase camera message throughput

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import sensor, image, time, math, pyb, os
 import machine
 
-def log_message(message):
-    with open('/logfile.log', 'a') as logfile:
-        timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-        logfile.write(f"{timestamp} - {message}\n")
-
 class USBReceiver:
     def __init__(self, usb):
         self.usb = usb
 
     def wait_for_connection(self):
         while not self.usb.isconnected():
-            log_message("Waiting for USB connection...")
             time.sleep(0.1)
 
     def read_line(self):
@@ -25,19 +19,17 @@ class USBReceiver:
                         break
                     line += char
                 except UnicodeDecodeError:
-                    log_message("Decode error: replacing unknown character.")
                     line += '?'
         return line.strip()
 
     def receive_params(self):
         self.wait_for_connection()
-        log_message("Connection established.")
         params = {}
         try:
             params['db_gain'] = self.read_line()
             params['gamma_corr'] = self.read_line()
          except Exception as e:
-             log_message(f"Error during file reception: {e}")
+             pass
         return params
 
 if __name__ == '__main__':
