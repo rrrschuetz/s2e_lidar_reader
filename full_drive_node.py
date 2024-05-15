02ac5b493a7da4b81ed9665b85d754392e99ba0e
Roland Schuetz <rrrschuetz@web.de>
2024-05-15 20:20:02 +0200

    increase camera message throughput

--- full_drive_node.py
+++ full_drive_node.py
@@ -198,7 +198,7 @@ class fullDriveNode(Node):
 
         with open("/home/rrrschuetz/ros2_ws4/src/s2e_lidar_reader/s2e_lidar_reader/h7_cam_exec.py", 'rb') as file:
             script_data = file.read()
-            header_data = f"{db_gain}\n{gamma_corr}\n{len(script_data)}\n".encode('utf-8')
+            header_data = f"{self.db_gain}\n{self.gamma_corr}\n{len(script_data)}\n".encode('utf-8')
             self.serial_port.write(header_data + script_data)
             self.get_logger().info(f"Script sent: {header_data}" )
     
