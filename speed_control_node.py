13c4ab48a322018ceb78b0c55fcdebab0968273b
Roland Schuetz <rrrschuetz@web.de>
2024-05-16 22:50:29 +0200

    increase camera message throughput

--- speed_control_node.py
+++ speed_control_node.py
@@ -129,7 +129,6 @@ class SpeedControlNode(Node):
             # self.y_pwm = self.neutral_pulse
             # self.y_pwm = self.min_y if self.reverse else self.max_y
         else:
-            #pid_output = self.pid(self.impulse_count) if self.impulse_count > 0 else 4.0
             pid_output = self.pid(self.impulse_count)
             self.get_logger().info(f"Impulses {self.impulse_count},pid_output {pid_output}")
             # Determine PWM adjustment based on PID output and desired direction.
