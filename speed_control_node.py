54eb59f680bfd53a3f9a5bed9846864ba7ef715f
Roland Schuetz <rrrschuetz@web.de>
2024-05-25 22:48:35 +0200

    increase camera message throughput

--- speed_control_node.py
+++ speed_control_node.py
@@ -108,7 +108,7 @@ class SpeedControlNode(Node):
         current_time = self.get_clock().now()
         self.impulse_history.append(1)
         self.impulse_history_long.append(current_time)
-        while self.impulse_history_long and (current_time - self.impulse_history_long[0] > self.rolling_average_period):
+        while self.impulse_history_long and (current_time - self.impulse_history_long[0] > self.rolling_avg_period):
             self.impulse_times.popleft()
 
     def set_speed_callback(self, msg):
