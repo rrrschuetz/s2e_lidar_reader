d2e99da4d1b1c3b84bdd3488cbfd1c819f118b6d
Roland Schuetz <rrrschuetz@web.de>
2024-05-29 23:11:23 +0200

    increase camera message throughput

--- WT61_node.py
+++ WT61_node.py
@@ -79,6 +79,8 @@ class ImuNode(Node):
             yaw, pitch, roll = [x / 32768.0 * 180 for x in angle_data]  # Convert to degrees
             quat = quaternion_from_euler(roll, pitch, yaw)
             self.publish_imu_orientation(quat)
+            #self.get_logger().info(f"Quaternion published: yaw: {yaw}, pitch: {pitch}, roll: {roll}")
+            #self.get_logger().info(f"Roll published: {roll}")
 
     def check_sum(self, packet):
         return sum(packet[:-1]) & 0xff == packet[-1]
@@ -92,8 +94,7 @@ class ImuNode(Node):
         imu_msg.header.frame_id = 'base_link'
         imu_msg.orientation = quat
         self.imu_pub.publish(imu_msg)
-        #self.get_logger().info(f"Quaternion published: yaw: {yaw}, pitch: {pitch}, roll: {roll}")
-        self.get_logger().info(f"Roll published: {roll}")
+        self.get_logger().info("Imu msg published.")
 
     def check_imu_data_timeout(self):
         if (self.get_clock().now() - self.last_received_time).nanoseconds > 1e9:
